0360eb9306a3e9f413726dcb1edb160292991776
remboinc <remboinc@mail.ru>
2023-10-01 23:34:23 +0300

    Delete True/False

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def main():
             response.raise_for_status()
             json = response.json()
             lesson_title = response.json().get('new_attempts')[0].get('lesson_title')
-            if json.get('new_attempts')[0].get('is_negative') is True:
+            if json.get('new_attempts')[0].get('is_negative'):
                 bot.send_message(text=f'Преподаватель проверил работу "{lesson_title}"\n\nЕсть над чем поработать',
                                  chat_id=chat_id)
-            elif json.get('new_attempts')[0].get('is_negative') is False:
+            elif not json.get('new_attempts')[0].get('is_negative'):
                 bot.send_message(text=f'Урок /"{lesson_title}/" сдан!',
                                  chat_id=chat_id)
             elif json.get('status') == 'timeout':
